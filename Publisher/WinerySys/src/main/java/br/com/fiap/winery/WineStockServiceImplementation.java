2d31195086fa4b10564fb356f091e4577e7b3b5e
letyresinaa <leticiaresina@gmail.com>
2025-05-12 16:06:14 -0300

    feat: implementation da interface winestockservice da publisher

---
+++ Publisher/WinerySys/src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -0,0 +1,28 @@
+package br.com.fiap.winery;
+
+import jakarta.jws.WebService;
+
+import java.util.Arrays;
+import java.util.List;
+
+@WebService(endpointInterface = "br.com.fiap.winery.WineStockService")
+public class WineStockServiceImplementation implements WineStockService {
+    List<String> vinhos = Arrays.asList("Cabernet", "Merlot", "Chardonnay");
+
+    @Override
+    public String getMenu() {
+        String menu = "";
+        for (int i = 0; i < vinhos.size(); i++) {
+            menu += vinhos.get(i);
+            if (i < vinhos.size() - 1) {
+                menu += ", ";
+            }
+        }
+        return menu;
+    }
+
+    @Override
+    public String placeOrder(String name, int quantity) {
+        return "Pedido confirmado!";
+    }
+}
